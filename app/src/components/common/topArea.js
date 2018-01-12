99923a35cb44b95943cca7fc2c8e0a8b21b36396
ylu021 <yichen_0@hotmail.com>
2018-01-11 23:49:10 -0500

    change double quote to single

--- app/src/components/common/topArea.js
+++ app/src/components/common/topArea.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './topArea.css';
 
 const TopArea = (props) => (
-  <div className="topArea">
+  <div className='topArea'>
     {props.children}
   </div>
 )
 
 export const CenterText = ({text}) => (
-  <div className="centerText">
+  <div className='centerText'>
     {text}
   </div>
 );
